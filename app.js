50389e53816436ff82df6c9d11b024515cf370c0
Khadija Tul Kubra <75326638+khadija12321@users.noreply.github.com>
2024-02-25 21:48:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+Array.from(
+    document.querySelectorAll(".navigate-price, .navigate-button")).forEach((item) => {
+        item.onclick = () => {
+            item.parentElement.parentElement.classList.toggle("change");
+        };
+    });
+
+
+// Add Event Litener in menu
+const container = document.querySelector(".container");
+const color =["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+document.querySelector(".open-navbar-icon").addEventListener('click', ()=>{
+    container.classList.add('change');
+});
+
+document.querySelector(".close-navbar-icon").addEventListener('click', ()=>{
+    container.classList.remove('change');
+});
+
+let i =0;
+
+Array.from(document.querySelectorAll('.nav-link')).forEach((item) => {
+    item.style.cssText = `background-color: ${color[i++]}`;
+});
